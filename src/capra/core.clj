dba20e117a23519e05daf6fa98b703f5023fa638
MikeHardIce <mikehardice@gmail.com>
2021-12-06 00:07:10 +0000

    some arity for the window create function

--- src/capra/core.clj
+++ src/capra/core.clj
@@ -5,8 +5,10 @@
            [javax.swing ImageIcon JFrame]))
 
 (defn create-window
-  ""
-  [x y width height title color]
+  "Creates and displays a window. Returns a map consisting of the window and the canvas"
+  ([x y width height title] (create-window x y width height title Color/white false))
+  ([x y width height title color] (create-window x y width height title color false))
+  ([x y width height title color resizable?]
   (let [dimension (Dimension. width height)
         canvas (doto (Canvas.)
                  (.setName title)
@@ -17,11 +19,13 @@
       (.setLayout (BorderLayout.))
       (.add canvas)
       (.setSize dimension)
-      (.setResizable true)
+      (.setResizable resizable?)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.addWindowListener (fn [frame _] (.dispose frame)))
       (.setName title)
       (.setTitle title)
       (.setLocation x y)
       (.setVisible true))
     (doto canvas
       (.requestFocus))
-    {:window window :canvas canvas}))
+    {:window window :canvas canvas})))
