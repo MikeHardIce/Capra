297fd6dbe637a1ad686b94d51ea70cfa0cd9af20
MikeHardIce <mikehardice@gmail.com>
2021-12-12 03:12:52 +0000

    some basic shapes

--- src/capra/core.clj
+++ src/capra/core.clj
@@ -1,8 +1,10 @@
 (ns capra.core
-  (:import [java.awt Color Dimension Graphics2D Canvas BorderLayout]
+  (:import [java.awt Color Dimension Graphics2D Canvas BorderLayout Rectangle BasicStroke]
            [java.awt.event InputEvent ComponentEvent KeyAdapter KeyEvent MouseAdapter MouseEvent MouseMotionAdapter WindowAdapter WindowEvent]
-           [java.awt.geom Ellipse2D Line2D Path2D Rectangle2D Point2D Arc2D]
+           [java.awt.geom Ellipse2D Ellipse2D$Double Line2D Line2D$Double Path2D Rectangle2D Point2D Arc2D]
            [javax.swing ImageIcon JFrame]))
+
+(set! *warn-on-reflection* true)
 
 (defn create-window
   "Creates and displays a window. Returns a map consisting of the window and the canvas"
@@ -13,7 +15,8 @@
         canvas (doto (Canvas.)
                  (.setName title)
                  (.setPreferredSize dimension)
-                 (.setBackground color))
+                 (.setBackground color)
+                 (.setIgnoreRepaint true))
         window (JFrame.)]
     (doto window
       (.setLayout (BorderLayout.))
@@ -21,7 +24,7 @@
       (.setSize dimension)
       (.setResizable resizable?)
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-      (.addWindowListener (fn [frame _] (.dispose frame)))
+      ;(.addWindowListener (fn [frame _] (.dispose frame)))
       (.setName title)
       (.setTitle title)
       (.setLocation x y)
@@ -29,3 +32,35 @@
     (doto canvas
       (.requestFocus))
     {:window window :canvas canvas})))
+
+(defn draw-rect 
+  [^Canvas canvas x y width height color fill?]
+  (let [^Rectangle2D rec (Rectangle. x y width height)
+        ^Graphics2D graphics (.getGraphics canvas)]
+    (.setColor graphics color)
+    (if fill? 
+      (.fill graphics rec)
+      (.draw graphics rec))
+    canvas))
+
+(defn draw-line
+  ([^Canvas canvas x0 y0 x1 y1 color] (draw-line canvas x0 y0 x1 y1 color 1.0))
+  ([^Canvas canvas x0 y0 x1 y1 color thickness]
+  (let [^Line2D line (Line2D$Double. x0 y0 x1 y1)
+        ^Graphics2D graphics (.getGraphics canvas)]
+    (.setStroke graphics (BasicStroke. thickness))
+    (.setColor graphics color)
+    (.draw graphics line))
+  canvas))
+
+(defn draw-ellipse
+  [^Canvas canvas x y width height color fill?]
+  (let [[x0 y0] [(- x (/ width 2)) (- y (/ height 2))]
+        ^Ellipse2D circle (Ellipse2D$Double. x0 y0 width height)
+        ^Graphics2D graphics (.getGraphics canvas)]
+    (.setColor graphics color)
+    (.setStroke graphics (BasicStroke. 1))
+    (if fill?
+      (.fill graphics circle)
+      (.draw graphics circle)))
+  canvas)
