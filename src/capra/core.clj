65ab94a16854799eeca217a9f497c15db80b44cc
MikeHardIce <mikehardice@gmail.com>
2021-12-12 03:34:23 +0000

    draw function to remove duplication

--- src/capra/core.clj
+++ src/capra/core.clj
@@ -1,5 +1,5 @@
 (ns capra.core
-  (:import [java.awt Color Dimension Graphics2D Canvas BorderLayout Rectangle BasicStroke]
+  (:import [java.awt Color Shape Dimension Graphics2D Canvas BorderLayout Rectangle BasicStroke]
            [java.awt.event InputEvent ComponentEvent KeyAdapter KeyEvent MouseAdapter MouseEvent MouseMotionAdapter WindowAdapter WindowEvent]
            [java.awt.geom Ellipse2D Ellipse2D$Double Line2D Line2D$Double Path2D Rectangle2D Point2D Arc2D]
            [javax.swing ImageIcon JFrame]))
@@ -33,34 +33,31 @@
       (.requestFocus))
     {:window window :canvas canvas})))
 
-(defn draw-rect 
-  [^Canvas canvas x y width height color fill?]
-  (let [^Rectangle2D rec (Rectangle. x y width height)
-        ^Graphics2D graphics (.getGraphics canvas)]
+(defn- draw 
+  [^Canvas canvas ^Shape shape color fill? thickness]
+  (let [^Graphics2D graphics (.getGraphics canvas)]
     (.setColor graphics color)
-    (if fill? 
-      (.fill graphics rec)
-      (.draw graphics rec))
-    canvas))
+    (.setStroke graphics (BasicStroke. thickness))
+    (if fill?
+      (.fill graphics shape)
+      (.draw graphics shape)))
+  canvas)
+
+(defn draw-rect 
+  ([^Canvas canvas x y width height color fill?] (draw-rect canvas x y width height color fill? 1))
+  ([^Canvas canvas x y width height color fill? thickness]
+  (let [^Rectangle2D rec (Rectangle. x y width height)]
+    (draw canvas rec color fill? thickness))))
 
 (defn draw-line
-  ([^Canvas canvas x0 y0 x1 y1 color] (draw-line canvas x0 y0 x1 y1 color 1.0))
+  ([^Canvas canvas x0 y0 x1 y1 color] (draw-line canvas x0 y0 x1 y1 color 1))
   ([^Canvas canvas x0 y0 x1 y1 color thickness]
-  (let [^Line2D line (Line2D$Double. x0 y0 x1 y1)
-        ^Graphics2D graphics (.getGraphics canvas)]
-    (.setStroke graphics (BasicStroke. thickness))
-    (.setColor graphics color)
-    (.draw graphics line))
-  canvas))
+  (let [^Line2D line (Line2D$Double. x0 y0 x1 y1)]
+    (draw canvas line color nil thickness))))
 
 (defn draw-ellipse
-  [^Canvas canvas x y width height color fill?]
+  ([^Canvas canvas x y width height color fill?] (draw-ellipse canvas x y width height color fill? 1))
+  ([^Canvas canvas x y width height color fill? thickness]
   (let [[x0 y0] [(- x (/ width 2)) (- y (/ height 2))]
-        ^Ellipse2D circle (Ellipse2D$Double. x0 y0 width height)
-        ^Graphics2D graphics (.getGraphics canvas)]
-    (.setColor graphics color)
-    (.setStroke graphics (BasicStroke. 1))
-    (if fill?
-      (.fill graphics circle)
-      (.draw graphics circle)))
-  canvas)
+        ^Ellipse2D circle (Ellipse2D$Double. x0 y0 width height)]
+    (draw canvas circle color fill? thickness))))
